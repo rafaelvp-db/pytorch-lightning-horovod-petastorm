9ab38edc02c177ba05c9259ad4d15dcf6e915a31
Rafael Pierre <rafael.pierre@databricks.com>
2022-11-15 21:40:38 +0100

    ds

--- carsifier/data/dataset.py
+++ carsifier/data/dataset.py
@@ -3,8 +3,20 @@ import numpy as np
 
 
 class Cars(StanfordCars):
-    def __init__(**kwargs):
-        super().__init__(kwargs)
+    def __init__(
+        root = "/tmp",
+        split = "train",
+        transform = None,
+        target_transform = None,
+        download = True
+    ):
+        super().__init__(
+            root,
+            split,
+            transform,
+            target_transform,
+            download
+        )
 
     def __getitem__(self, idx):
 
